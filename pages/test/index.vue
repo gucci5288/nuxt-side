14706541af1d03a71d336a92f727321946514569
gucci.ku <gucci.ku@solartnine.com>
2020-09-15 13:39:00 +0800

    init commit

---
+++ pages/test/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="md:flex bg-white rounded-lg p-6">
+    <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="avatar.jpg">
+    <div class="text-center md:text-left">
+      <h2 class="text-lg">Erin Lindford</h2>
+      <div class="text-purple-500">Product Engineer</div>
+      <div class="text-gray-600">erinlindford@example.com</div>
+      <div class="text-gray-600">(555) 765-4321</div>
+    </div>
+
+
+
+    <div class="box-border h-20 w-32 p-4 border-4 border-gray-400 bg-gray-200">
+      <div class="h-full w-full bg-gray-400"></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
