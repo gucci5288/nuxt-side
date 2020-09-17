aea606dc7418b40b73fcefd4089b52db14702aed
gucci.ku <gucci.ku@solartnine.com>
2020-09-17 17:27:54 +0800

    test for Maintainability concerns @apply feature

---
+++ pages/scroll/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg">
+    <div class="page bg-gray-100">Page1</div>
+    <div class="page bg-gray-300">Page2</div>
+    <div class="page bg-gray-500">Page3</div>
+    <div class="page bg-gray-700">Page4</div>
+    <div class="page bg-gray-800">Page5</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Scroll",
+  created() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods:{
+    handleScroll(e){
+      const doc = document.documentElement
+      const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0)
+      console.log('onScroll',top)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.page
+  @apply h-screen flex justify-center items-center
+</style>
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="md:flex bg-white rounded-lg p-6">
-    <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="avatar.jpg">
-    <div class="text-center md:text-left">
-      <h2 class="text-lg">Erin Lindford</h2>
-      <div class="text-purple-500">Product Engineer</div>
-      <div class="text-gray-600">erinlindford@example.com</div>
-      <div class="text-gray-600">(555) 765-4321</div>
+  <div>
+    <div>
+      <div class="md:flex bg-white rounded-lg p-6">
+        <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="avatar.jpg">
+        <div class="text-center md:text-left">
+          <h2 class="text-lg">Erin Lindford</h2>
+          <div class="text-purple-500">Product Engineer</div>
+          <div class="text-gray-600">erinlindford@example.com</div>
+          <div class="text-gray-600">(555) 765-4321</div>
+        </div>
+        <div class="box-border md:box-content h-20 w-32 p-4 border-4 border-gray-400 bg-gray-200">
+          <div class="h-full w-full bg-gray-400"></div>
+        </div>
+      </div>
     </div>
 
-
-
-    <div class="box-border h-20 w-32 p-4 border-4 border-gray-400 bg-gray-200">
-      <div class="h-full w-full bg-gray-400"></div>
+    <div class="container mx-auto h-20 bg-red-200 px-4" >
+      Hello world
     </div>
-
   </div>
 </template>
 
